cc21f5542a6ea26a7d4c44526821a63524049698
Jyoshna-15 <jyoshna.d.n@gmail.com>
2026-09-03 14:23:09 +0530

    Add Firebase push notifications and fcmToken field

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { signup, login } from "../controllers/authController";
+import { signup, login,updateFcmToken } from "../controllers/authController";
+import { protect,  } from "../middleware/authMiddleware";
 
 const router = Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/fcm-token", protect, updateFcmToken);
 
 export default router;
